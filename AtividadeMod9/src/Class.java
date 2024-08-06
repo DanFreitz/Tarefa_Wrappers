c0fbd21fb9b61609fc4124126745fcc3aeb8274a
unknown <danielaugusto.8@outlook.com>
2024-08-06 10:19:26 -0300

    Atividade Wrappers

---
+++ AtividadeMod9/src/Class.java
@@ -0,0 +1,17 @@
+/**
+ * @author Danfreitz
+ */
+
+public class Class {
+
+    public static void main(String args[]) {
+
+        int x = 10;
+        double y = (double) x;
+        System.out.println(y);
+
+        double z = 10.0;
+        Double w = (Double) z;
+
+    }
+}
